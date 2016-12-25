c805cc385607ce8d4f605bf85f43d9213550d8c0
Gnucki <tprelot@gmail.com>
2016-12-25 12:16:59 +0100

    Upgrade eslint rules

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ module.exports = {
   extends: 'airbnb-base',
   env: {
       browser: true,
+      es6: true,
+      jasmine: true,
+      mocha: true,
       node: true
   },
   parserOptions: {
     sourceType: 'script'
   },
   rules: {
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'no-plusplus': 'off',
+    'no-underscore-dangle': ['error', {allowAfterThis: true}],
+    'object-curly-spacing': ['error', 'never']
   }
 };
