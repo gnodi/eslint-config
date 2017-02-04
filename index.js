dd0277a635c5afaef7c0771fccbf4ce6160f0a71
Gnucki <tprelot@gmail.com>
2017-02-04 17:07:22 +0100

    Add lint rule for function definition

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 module.exports = {
   extends: 'airbnb-base',
   env: {
-      browser: true,
-      es6: true,
-      jasmine: true,
-      mocha: true,
-      node: true
+    browser: true,
+    es6: true,
+    jasmine: true,
+    mocha: true,
+    node: true
   },
   parserOptions: {
     sourceType: 'script'
@@ -16,6 +16,7 @@ module.exports = {
     'comma-dangle': ['error', 'never'],
     'no-plusplus': 'off',
     'no-underscore-dangle': ['error', {allowAfterThis: true}],
+    'no-use-before-define': ['error', {functions: false}],
     'object-curly-spacing': ['error', 'never']
   }
 };
